fef546f34f4c20ff75824e95eeea47498d2d626a
Adam Hoffman <wintros@yahoo.com>
2019-09-19 09:43:30 -0600

    cookies

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets.js')
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({message: 'You shall not pass'})
+      } else {
+        next()
+      }
+    })
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          req.user = {username: decodedToken.username}
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Ran into an unexpected error' });
+      });
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+};
